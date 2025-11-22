1556bfebfe1d27d4eb2eed2936540349885a290a
Maksym Bondarchuk <belkamax05@gmail.com>
2025-11-22 01:28:55 +0000

    sync

--- include.zsh
+++ include.zsh
@@ -3,6 +3,9 @@ __CURRENT_DIR=$(realpath $(dirname $__CURRENT_FILE))
 
 SHULKER_CLI_PROMPT_DIR=$__CURRENT_DIR
 
+export ZAP_DISABLE_AUTO_INSTALL="1"
+export ZAP_DISABLE_PACKAGES="1"
+
 # echo "Prompt TODO at $SHULKER_CLI_PROMPT_DIR"
 # local prompt_type = "p10k" --? Possible options are "p10k", "kali", "zap", "custom"
 PROMPT_TYPE="${SHULKER_PROMPT_TYPE:-zap}"
